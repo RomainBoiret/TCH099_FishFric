4081a7a3b3ddd1c3e6cb2922bba5cec17a65ecfc
Romain <romainboiret@gmail.com>
2024-04-07 12:16:00 -0400

    feature/page_faq

---
+++ Site_web/FAQ/Scripts/script.js
@@ -0,0 +1,12 @@
+// toggle icon navigation
+let menuIcon = document.querySelector('#menu-icon');
+let navigation = document.querySelector('.navigation');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navigation.classList.toggle('active');
+}
+
+// remove toggle icon and navbar when click navigation links (scroll)
+menuIcon.classList.remove('bx-x');
+navigation.classList.remove('active');
