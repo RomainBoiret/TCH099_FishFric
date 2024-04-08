3a4abd2953ae84090ec316b08fbfbd289e20b290
Romain <romainboiret@gmail.com>
2024-04-07 23:34:39 -0400

    petit ajout

--- Site_web/demanderSupport/scripts/script.js
+++ Site_web/demanderSupport/scripts/script.js
@@ -3,7 +3,7 @@ var btnSoumettre = document.getElementById("soumettre");
 btnSoumettre.addEventListener("click", function() {
     
     Swal.fire({
-        imageUrl: "",
+        imageUrl: "https://github.com/RomainBoiret/TCH099_FishFric/blob/main/Site_web/demanderSupport/Images/queue-baleine.png?raw=true",
         imageHeight: 80,
         title: "Message envoyé !",
         text: "Votre demande de support a été soumise avec succès !",
