4ff854b6ad2d7b316418b692ad01e391488f7b7a
Martinrolo <martinrolod@hotmail.com>
2024-04-11 11:57:11 -0400

    test régler bug modif courriel

--- Site_web/pageListeCompte/API/preferences.php
+++ Site_web/pageListeCompte/API/preferences.php
@@ -46,7 +46,7 @@ if(isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "PUT"){
         //S'il n'y a pas d'erreurs, on peut effectuer le changement de courriel
         if(empty($erreurs)) {
             //Altérer le courriel de l'utilisateur
-            $requete = $conn->prepare("UPDATE Compte SET courriel = '$nouveauCourriel' WHERE id = $idUtilisateur;");
+            $requete = $conn->prepare("UPDATE Compte SET courriel = $nouveauCourriel WHERE id = $idUtilisateur;");
             $requete->execute();
 
             echo json_encode(['msgSucces' => "Le courriel a bien été modifié."]);
