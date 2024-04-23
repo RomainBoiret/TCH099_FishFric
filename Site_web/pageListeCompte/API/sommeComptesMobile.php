8f2a1a8f343861a2fd0274b0d39d0360f48a75d3
FelixCaronn <148878676+FelixCaronn@users.noreply.github.com>
2024-04-23 18:12:07 -0400

    ajout updateSolde pour mobile

--- Site_web/pageListeCompte/API/sommeComptesMobile.php
+++ Site_web/pageListeCompte/API/sommeComptesMobile.php
@@ -2,39 +2,70 @@
 
     if(isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "POST"){
 
-        //Gérer la connexion à la base de données
-        try {
-            require "../../connexion.php";
-        } catch(Exception $e) {
-            die("Connexion échouée!: " .$e->getMessage());
-        }
-
-        $donneesJSON = json_decode(file_get_contents("php://input"), true);
-
-        if(!empty($donneesJSON))
+        //Update Solde total du compte apres une transaction
+        if (preg_match('/\/pageListeCompte\/API\/gestionTransfertmobile\.php\/updateSolde$/', $_SERVER['REQUEST_URI'], $matches))
         {
-
-            $idUser = trim(implode($donneesJSON['idUser']));
-
-            $requete = $conn->prepare("SELECT * FROM SommeTotale WHERE compteId = $idUser AND dateSolde > CURRENT_DATE-7 ORDER BY dateSolde");
-            $requete->execute();
-            $sommes = $requete->fetchAll(PDO::FETCH_ASSOC);
-
-            // Échapper les caractères spéciaux dans le contenu des comptes
-            foreach ($sommes as $somme) {
-                $somme['compteId'] = htmlspecialchars($somme['compteId'], ENT_QUOTES, 'UTF-8');
-                $somme['solde'] = htmlspecialchars($somme['solde'], ENT_QUOTES, 'UTF-8');
-                $somme['dateSolde'] = htmlspecialchars($somme['dateSolde'], ENT_QUOTES, 'UTF-8');
+            //Gérer la connexion à la base de données
+            try {
+                require "../../connexion.php";
+            } catch(Exception $e) {
+                die("Connexion échouée!: " .$e->getMessage());
             }
 
-            
-            //Encoder les informations des comptes en json
-            echo json_encode(["comptes" => $sommes, "code"=>"200"]);
+            $donneesJSON = json_decode(file_get_contents("php://input"), true);
+
+            if(!empty($donneesJSON))
+            {
+                $soldeTotal = trim(implode($donneesJSON['solde']));
+                $idUser = trim(implode($donneesJSON['id']));
+                if(isset($soldeTotal))
+                {
+                    $requete = $conn->prepare("UPDATE SommeTotale SET solde = $soldeTotal WHERE compteId = $idUser AND dateSolde = CURRENT_DATE");
+                    $requete->execute();
+                }
+            }
+            else
+            {
+                echo json_encode(["erreur"=>"Erreur lors de la requete", "code"=>"400"]);
+            }
         }
         else
         {
-            echo json_encode(['erreur'=>"Erreur lors de l'envoi de donnees", 'code'=>"400"]);
+            //Gérer la connexion à la base de données
+            try {
+                require "../../connexion.php";
+            } catch(Exception $e) {
+                die("Connexion échouée!: " .$e->getMessage());
+            }
+
+            $donneesJSON = json_decode(file_get_contents("php://input"), true);
+
+            if(!empty($donneesJSON))
+            {
+
+                $idUser = trim(implode($donneesJSON['idUser']));
+
+                $requete = $conn->prepare("SELECT * FROM SommeTotale WHERE compteId = $idUser AND dateSolde > CURRENT_DATE-7 ORDER BY dateSolde");
+                $requete->execute();
+                $sommes = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+                // Échapper les caractères spéciaux dans le contenu des comptes
+                foreach ($sommes as $somme) {
+                    $somme['compteId'] = htmlspecialchars($somme['compteId'], ENT_QUOTES, 'UTF-8');
+                    $somme['solde'] = htmlspecialchars($somme['solde'], ENT_QUOTES, 'UTF-8');
+                    $somme['dateSolde'] = htmlspecialchars($somme['dateSolde'], ENT_QUOTES, 'UTF-8');
+                }
+
+                
+                //Encoder les informations des comptes en json
+                echo json_encode(["comptes" => $sommes, "code"=>"200"]);
+            }
+            else
+            {
+                echo json_encode(['erreur'=>"Erreur lors de l'envoi de donnees", 'code'=>"400"]);
+            }
         }
+
 
     }
 
